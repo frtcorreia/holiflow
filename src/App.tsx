27f96136d76cac0f2db9cce18d53c9a94895d4b8
Filipe <frtcorreia@gmail.com>
2025-03-04 16:44:44 +0000

    remove landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authStore";
-import LandingPage from "./screens/LandingPage";
 import SignInScreen from "./screens/SignInScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import PasswordRecoveryScreen from "./screens/PasswordRecoveryScreen";
